8b8785c6fc8323cd92e0d1e0bb439d1aa077dd8c
Mike Pontillo <mpontillo@spectrum42.com>
2019-08-27 12:15:56 -0700

    Test an alternate way to get interface addresses.

--- pcap_test.go
+++ pcap_test.go
@@ -1,8 +1,26 @@
 package pcap
 
-import "testing"
+import (
+	"fmt"
+	"net"
+	"testing"
+)
 
 func TestGetInterfaces(t *testing.T) {
 	// No special privileges should be needed to call this.
 	GetInterfaces()
+}
+
+func TestNetGetInterfaces(t *testing.T) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		t.FailNow()
+	}
+	for _, iface := range interfaces {
+		fmt.Printf("Interface: %+v\n", iface)
+		addrs, _ := iface.Addrs()
+		fmt.Printf("Addresses: %+v\n", addrs)
+		maddrs, _ := iface.MulticastAddrs()
+		fmt.Printf("Multicast addresses: %+v\n\n", maddrs)
+	}
 }
